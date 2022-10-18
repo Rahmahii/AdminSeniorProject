26414ec768e24930aeeb1fdf4596b9ba76f86561
رحمة أبوجياب <abujayyabr@gmail.com>
2022-10-18 23:00:55 +0300

    initial commit

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ app.set("view engine","ejs")
 app.set("views","views")//defult views
  
 //app.use(express.static(path.join(__dirname, 'static')))
+app.get("/signUp",(req,res,next)=>{
+    //res.render( "signUp",{titel:"signUp"}) 
+
+    fetch(' http://localhost:3000/store/getStoresNames').then(res => res.json())
+        .then(json => {    
+            res.render( "signUp",{json,titel:"signUp"})
+        })
+    
+    console.log(req.query)
+})
 
 app.get("/Products", (req, res, next) =>{
     
     fetch('http://localhost:3000/product/StorAdminView', {
-	method: 'post',
-	body: JSON.stringify({"storeId":1}),
+	method: 'get',
 	headers: {'Content-Type': 'application/json'}}).then(res => res.json())
     .then(json => {    
         res.render( "Products",{json,titel:"Products"})
@@ -42,7 +51,20 @@ app.get("/Customer", (req, res, next) =>{
 })
 app.get("/Dashboard", (req, res, next) =>{
    
-    res.render( "Dashboard",{titel:"Dashboard"})
+     
+    fetch('http://localhost:3000/invoice/dashboard', {
+        method: 'post',
+        body: JSON.stringify({"storeId":1}),
+        headers: {'Content-Type': 'application/json'}}).then(res => res.json())
+        .then(json => { 
+            fetch('http://localhost:3000/invoice/dashboard2', {
+                method: 'post',
+                body: JSON.stringify({"storeId":1}),
+                headers: {'Content-Type': 'application/json'}}).then(res => res.json())
+                .then(json2 => { 
+                    res.render( "Dashboard",{json,json2,titel:"Dashboard"})
+                })
+        })
 
 })
 
