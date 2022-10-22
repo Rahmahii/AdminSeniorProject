1db9d44dac598206cdebac1fc0ac1cb6c80689f0
رحمة أبوجياب <abujayyabr@gmail.com>
2022-10-22 17:18:04 +0300

    initial commit

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const app = express()
 app.use(express.urlencoded({extended: true}));
 app.set("view engine","ejs")
 app.set("views","views")//defult views
+const jwt = require('jsonwebtoken');
+const atob = require("atob");
  
 //app.use(express.static(path.join(__dirname, 'static')))
 
 
-app.use(cookieParser('NotSoSecret'));
+
+app.use(cookieParser());
+
+//app.use(cookieParser('NotSoSecret'));
+
 app.use(session({
   secret : 'something',
   cookie: { maxAge: 60000 },
@@ -22,18 +28,49 @@ app.use(session({
 app.use(flash());
 
 
-app.get("/signUp",(req,res,next)=>{
-    //res.render( "signUp",{titel:"signUp"}) 
+app.get("/SignUp",(req,res,next)=>{
 
-    fetch(' http://localhost:3000/store/getStoresNames').then(res => res.json())
+        fetch(' http://localhost:3000/store/getStoresNames').then(res => res.json())
         .then(json => {   
             const message=req.flash('message')
-            res.render( "signUp",{json,message,titel:"signUp"})
+            res.render( "SignUp",{json,message,titel:"SignUp"})
 })
 })
-//console.log(isSaved)
+app.get("/Login",(req,res,next)=>{
+    const message=req.flash('message')
+        res.render( "Login",{message,titel:"Login"})
 
-app.get("/waiting", (req, res, next) =>{
+})
+
+app.get("/waiting",(req,res,next)=>{
+    fetch('http://localhost:3000/user/loginAdmin', {
+        method: 'post',
+        body: JSON.stringify({"email":req.query.email,"password":req.query.password}),
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include' }).then(res => res.json(),
+        )      
+        .then(json => {   
+          //  console.log(res.raw['set-cookie'])
+          if(json.status==true){ 
+            req.flash('message', json.message)
+            req.flash('token', json.token)
+            res.redirect( "/Dashboard") 
+        }else if(json.status=="waiting"){
+           var message= json.message
+           console.log(message)
+            res.render( "waiting",{message,titel:"waiting"}) 
+          }
+          else{
+            req.flash('message', json.message)
+            res.redirect( "/Login")
+          }
+          
+        })
+ 
+
+})
+
+app.get("/checking", (req, res, next) =>{
     
     fetch('http://localhost:3000/user/signUpAdmin', {
         method: 'post',
@@ -41,12 +78,12 @@ app.get("/waiting", (req, res, next) =>{
         headers: {'Content-Type': 'application/json'}}).then(res => res.json())
         .then(json => {   
           if(json.status){ 
-            res.render( "waiting",{titel:"waiting"})
-            console.log(json.status)
+            req.flash('message', "Your account created successfully")
+            res.redirect( "/Login")
+           
           }else{
             req.flash('message', json.message)
-            res.redirect( "/signUp")
-            //console.log(json.message)
+            res.redirect( "/SignUp")
           }
           
         })
@@ -54,22 +91,31 @@ app.get("/waiting", (req, res, next) =>{
 
 
 
-app.get("/Products", (req, res, next) =>{
+    // console.log(JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()))
     
-    fetch('http://localhost:3000/product/StorAdminView', {
-	method: 'post',
-    body: JSON.stringify({"storeId":1}),
-	headers: {'Content-Type': 'application/json'}}).then(res => res.json())
-    .then(json => {    
+    //console.log(token)
+    app.get("/Products", (req, res, next) =>{ 
+        var storeId=req.flash('storeId')
+        req.flash('storeId',storeId)
+        fetch('http://localhost:3000/product/StorAdminView', {
+            method: 'post',
+            body: JSON.stringify({"storeId":storeId}),
+            headers: {'Content-Type': 'application/json'}}).then(res => res.json())
+            .then(json => {    
+        
         res.render( "Products",{json,titel:"Products"})
     })
 
 })
 
 app.get("/Admins", (req, res, next) =>{
+    var storeId=req.flash('storeId')
+    req.flash('storeId',storeId)
+  
+//console.log(token)
     fetch('http://localhost:3000/user/getUserByStore', {
         method: 'post',
-        body: JSON.stringify({"storeId":1}),
+        body: JSON.stringify({"storeId":storeId}),
         headers: {'Content-Type': 'application/json'}}).then(res => res.json())
         .then(json => {  
            res.render( "Admins",{json,titel:"Admins"})
@@ -77,9 +123,11 @@ app.get("/Admins", (req, res, next) =>{
 })
 
 app.get("/Customer", (req, res, next) =>{
+    var storeId=req.flash('storeId')
+    req.flash('storeId',storeId)
     fetch('http://localhost:3000/invoice/getStoreInvoices', {
         method: 'post',
-        body: JSON.stringify({"storeId":1}),
+        body: JSON.stringify({"storeId":storeId}),
         headers: {'Content-Type': 'application/json'}}).then(res => res.json())
         .then(json => {  
             res.render( "Customers",{json,titel:"Customers"})
@@ -88,11 +136,18 @@ app.get("/Customer", (req, res, next) =>{
    
 })
 app.get("/Dashboard", (req, res, next) =>{
-   
-     
+    const token=req.flash('token')
+    if(token!=0){
+    const token1 = token.toString().split(".")[1];
+    var storeId = JSON.parse(atob(token1)).storeId;
+}else{
+    var storeId=req.flash('storeId')
+}
+   req.flash('storeId', storeId)
+       
     fetch('http://localhost:3000/invoice/dashboard', {
         method: 'post',
-        body: JSON.stringify({"storeId":1}),
+        body: JSON.stringify({"storeId":storeId}),
         headers: {'Content-Type': 'application/json'}}).then(res => res.json())
         .then(json => { 
             fetch('http://localhost:3000/invoice/dashboard2', {
@@ -100,7 +155,8 @@ app.get("/Dashboard", (req, res, next) =>{
                 body: JSON.stringify({"storeId":1}),
                 headers: {'Content-Type': 'application/json'}}).then(res => res.json())
                 .then(json2 => { 
-                    res.render( "Dashboard",{json,json2,titel:"Dashboard"})
+                    const message=req.flash('message')
+                    res.render( "Dashboard",{json,json2,message,token,titel:"Dashboard"})
                 })
         })
 
